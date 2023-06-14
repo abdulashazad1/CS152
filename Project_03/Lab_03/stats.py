9754f8c17eb7794c1a4e9b7c9eb5662c3d16f935
abdulashazad1 <97552053+abdulashazad1@users.noreply.github.com>
2023-06-14 13:51:46 -0400

    Update stats.py

--- Project_03/Lab_03/stats.py
+++ Project_03/Lab_03/stats.py
@@ -3,16 +3,16 @@
 CS 152
 LAB_03'''
 
-#Making a function that takes a list of numbers and returns their sum
+#making a function that takes a list of numbers and returns their sum
 def sum(numbers):
     total = 0.0 #variable to hold the sum. It also initializes sum to 0.0, a float.
     for number in numbers:
         total += float(number) #adds each number to 0.0, effectively making total hold the sum of all numbers
 
-    return total #returns the sum (the final value of total after the  for loop)
+    return total #returns the sum (the final value of total after the for loop)
 
 
-#Testing my function
+#testing my function
 def test():
     list1 = [1, 2, 3, 4] #declaring the list to be summed in sum(numbers)
     caller = sum(list1) #variable to hold the sum of list1
